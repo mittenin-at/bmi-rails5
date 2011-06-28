ff339f8f4b1b29fcdfd4ac2cfed522b527b49583
Stefan Haslinger <stefan@rantamplan>
2011-06-28 22:34:25 +0200

    Skalierung Vergleich 2
    # To cancel this commit, close this file without making a change. To commit,
    # write your message, then save and close the file.
    #
    # On branch master
    # Changes to be committed:
    #   (use "git reset HEAD <file>..." to unstage)
    #
    #	modified:   app/controllers/weighings_controller.rb
    #

--- app/controllers/weighings_controller.rb
+++ app/controllers/weighings_controller.rb
@@ -83,10 +83,10 @@ class WeighingsController < ApplicationController
 #      @minbmi = 15
 #      @maxbmi = 45
       @minbmi = [( Weighing.minimum(:weight, :conditions => ['user_id = ?', session[:user_id]]) * 100 * 100 / @height / @height ),
-                 ( Weighing.minimum(:weight, :conditions => ['user_id = ?', params[:competitor][:id]]) * 100 * 100 / @height_competitor / @height_competitor )].min.to_i - 1
+                 ( Weighing.minimum(:weight, :conditions => ['user_id = ?', params[:competitor][:id]]) * 100 * 100 / @height_competitor / @height_competitor )].min.to_i
       
       @maxbmi = [( Weighing.maximum(:weight, :conditions => ['user_id = ?', session[:user_id]]) * 100 * 100 / @height / @height ),
-                 ( Weighing.minimum(:weight, :conditions => ['user_id = ?', params[:competitor][:id]]) * 100 * 100 / @height_competitor / @height_competitor )].max.to_i + 2
+                 ( Weighing.minimum(:weight, :conditions => ['user_id = ?', params[:competitor][:id]]) * 100 * 100 / @height_competitor / @height_competitor )].max.to_i + 1
     end
   end
 
