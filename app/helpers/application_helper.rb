232e50b40b09d032a669cd60a1d92ee737d13970
Stefan Haslinger <stefan@informatom-1.(none)>
2012-08-02 12:17:37 +0200

    10 tests, web app theme update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,5 +6,13 @@ module ApplicationHelper
       return false
     end
   end
+
+  def current_user
+    if session[:user_id]
+      User.find_by_id(session[:user_id])
+    else
+      return false
+    end
+  end
 end
 
--- spec/views/weighings/compare.html.erb_spec.rb
+++ spec/views/weighings/compare.html.erb_spec.rb
@@ -13,8 +13,19 @@ describe "weighing/compare.html.html.erb", :type => :spec do
     login(@michael)
     enter_weighing(100, @date)
     visit select_competitor_weighings_path
+    page.should have_selector("option", text: @michael.email)
     page.select @stefan.email, :from => "competitor_id"
     click_button "Vergleichen"
     page.current_path.should == compare_weighings_path
+  end
+
+  it "should not be possible to compare to a private user's diagram" do
+    login(@angsthase)
+    enter_weighing(118, @date)
+    visit "/logout"
+    login(@michael)
+    enter_weighing(100, @date)
+    visit select_competitor_weighings_path
+    page.should_not have_selector("option", text: @angsthase.email)
   end
 end
