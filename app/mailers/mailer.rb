9e4b373d0b0e466c28361849a5c8249be388cee9
Stefan Haslinger <stefan.haslinger@mittenin.at>
2013-02-11 14:00:04 +0100

    Mailer

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,12 +1,6 @@
 class Mailer < ActionMailer::Base
   default :from => "einladung@bmi.mittenin.at"
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.mailer.invitation.subject
-  #
-
   def invitation(invitation, signup_url)
     @signup_url = signup_url
    	mail(:to => "#{invitation.recipient_email} <#{invitation.recipient_email}>", :subject => 'Einladung')
