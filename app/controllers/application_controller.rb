22efc64f3c1f5af15500cd5f27fbd8235e7d6e04
Stefan Haslinger <stefan.haslinger@mittenin.at>
2018-10-30 12:49:01 +0100

    updating less-rails, sass-rails, removed logging of defailt_url_options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,10 @@ def set_locale
   I18n.locale = params[:locale] || I18n.default_locale
 end
 
-def default_url_options(options={})
-  logger.debug "default_url_options is passed options: #{options.inspect}\n"
-  { :locale => I18n.locale }
-end
+#def default_url_options(options={})
+#  logger.debug "default_url_options is passed options: #{options.inspect}\n"
+#  { :locale => I18n.locale }
+#end
 
 protected
   def authorize
