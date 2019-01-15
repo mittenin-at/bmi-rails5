488f6805fb1d65297b22ae44a85920ca14d7ce66
stefan Haslinger <mail@stefan-haslinger.at>
2019-01-15 13:19:04 +0100

    upgrade to rails 5.2.2 complete

--- Gemfile
+++ Gemfile
@@ -1,17 +1,16 @@
 source 'http://rubygems.org'
 
-gem "rails", "~> 5.1.0"
+gem "rails", "5.2.2"
 gem 'mysql2'
 gem 'kaminari'
 gem 'bootstrap-kaminari-views'
 gem 'nokogiri'
-
+gem 'bootsnap'
 gem 'jquery-rails'
 gem "less-rails"
 gem "twitter-bootstrap-rails", git: 'git://github.com/seyhunak/twitter-bootstrap-rails.git'
 gem 'jquery-datatables-rails'
 gem 'bootstrap-datepicker-rails'
-
 gem 'sass-rails'
 gem 'coffee-rails'
 gem 'therubyracer'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,24 +3,24 @@ Rails.application.configure do
   config.eager_load = false
   config.consider_all_requests_local = true
 
-  if Rails.root.join('tmp/caching-dev.txt').exist?
+  if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
     config.cache_store = :null_store
   end
 
-  # config.active_storage.service = :local
+  config.active_storage.service = :local
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
-  #config.active_record.verbose_query_logs = true
+  config.active_record.verbose_query_logs = true
   config.assets.debug = true
   config.assets.quiet = true
   # config.action_view.raise_on_missing_translations = true
