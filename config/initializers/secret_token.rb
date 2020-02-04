2703c062d0b7285bf321c1befe1a59e71c4ba6a8
stefan Haslinger <mail@stefan-haslinger.at>
2020-02-04 09:52:06 +0100

    removed secret token (and changed it on server ;-) )

--- config/initializers/secret_token.rb
+++
@@ -1,9 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Bmi::Application.config.secret_token = 'e78a99c0bc2d8a4ee3fbade711ee83fe4ca8b41872710ee50aad6fec968fc9f5f230fec25953744757039ba3c6df16d1c54c5c14752cd4fda5cb787dbbb25ed4'
-
-Bmi::Application.config.secret_key_base = 'd36533de371a7c9ed77f9202569315dea49bec9f72e59e225989fde117446f5ec0f9990cd43c1e335c66051c02632ade03aab0883727019048e2f7b93ebd3016'
